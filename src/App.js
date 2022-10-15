e583cb15935f193d5c34b917e9c2c5409a9b37ce
Myranda Palomino <myranda.palomino@gmail.com>
2022-10-15 11:04:16 -0700

    create-react-app, installed material-ui, removed unused react logos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+     <h2>Hello World</h2>
+  );
+}
+
+export default App;
