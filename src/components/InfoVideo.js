b9c2b951d015295d552ee0822ff86cda2e5bfddb
Myranda Palomino <myranda.palomino@gmail.com>
2022-10-16 18:21:22 -0700

    youtube component

--- src/components/InfoVideo.js
+++ src/components/InfoVideo.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import { appData } from '../config/data';
+// import { appData } from '../config/data';
 
-export default function InfoVideo(props) {
-const { selected } = props;
-    console.log(appData[selected]);
-
-
+// export default function InfoVideo(props) {
+// const { selected } = props;
+//     console.log(appData[selected]);
 
 
-    return (
-        <div>
-            This is for Video.
-        </div>
-    )
-}
+
+
+//     return (
+//         <div>
+//             This is for Video.
+//         </div>
+//     )
+// }
 
 // update youtube video
+
+import YouTube from 'react-youtube';
+
+class InfoVideo extends React.Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+    return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+}
+
+export default <InfoVideo />
