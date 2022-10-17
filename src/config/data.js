92d3b99caa45b0138c0bc82a44bda9549012baf9
Myranda Palomino <myranda.palomino@gmail.com>
2022-10-16 18:45:45 -0700

    added video ids

--- src/config/data.js
+++ src/config/data.js
@@ -1,23 +1,58 @@
 export const appData = {
     'Ballet': {
-        videoId: 123,
+        videoId: ['Jb4-Mul_DmA', '9C0eBYW_2fg', '077BgT3h6As'],
     },
     'Jazz': {
-        videoId: 123,
+        videoId: ['KmVMInvINt4', 'E8ZuVYd_9_w', 'ZLd-uB3L5_Y'],
     },
     'Lyrical': {
-        videoId: 123,
+        videoId: ['r2k7wT50E24', 'gaGlykpusnM', 'UtJW_wd5Qe8'],
     },
     'Contemporary': {
-        videoId: 123,
+        videoId: ['uFojmxW_5Dw', 'oDSuuAeM10k', 'E4_a5me7BD0'],
     },
     'Tap': {
-        videoId: 123,
+        videoId: ['4PZcZpGTHxo', 'zHcd3OsqxFk', 'c97O-0akyYg'],
     },
     "Ballroom": {
-        videoId: 123,
+        videoId: ['5zUiy2CnVnE', 'xsJL_7URL0w', 'a6qwfCjBhDI'],
     },
     'Hip-Hop': {
-        videoId: 123,
+        videoId: ['1WIA6Yvj8Yg', 'iQuID3_5iDA', 'EYyOeZrMI9U'],
     },
   };
+
+//   Ballet
+// https://www.youtube.com/watch?v=Jb4-Mul_DmA
+// https://www.youtube.com/watch?v=9C0eBYW_2fg
+// https://www.youtube.com/watch?v=077BgT3h6As
+
+// Jazz
+// https://www.youtube.com/watch?v=KmVMInvINt4
+// https://www.youtube.com/watch?v=E8ZuVYd_9_w
+// https://www.youtube.com/watch?v=ZLd-uB3L5_Y
+
+// Lyrical
+// https://www.youtube.com/watch?v=r2k7wT50E24
+// https://www.youtube.com/watch?v=gaGlykpusnM
+// https://www.youtube.com/watch?v=UtJW_wd5Qe8
+
+// Contemporary
+// https://www.youtube.com/watch?v=uFojmxW_5Dw
+// https://www.youtube.com/watch?v=oDSuuAeM10k
+// https://www.youtube.com/watch?v=E4_a5me7BD0
+
+// Tap
+// https://www.youtube.com/watch?v=4PZcZpGTHxo
+// https://www.youtube.com/watch?v=zHcd3OsqxFk
+// https://www.youtube.com/watch?v=c97O-0akyYg
+
+// Ballroom
+// https://www.youtube.com/watch?v=5zUiy2CnVnE
+// https://www.youtube.com/watch?v=xsJL_7URL0w
+// https://www.youtube.com/watch?v=a6qwfCjBhDI
+
+// Hip-Hop
+// https://www.youtube.com/watch?v=1WIA6Yvj8Yg
+// https://www.youtube.com/watch?v=iQuID3_5iDA
+// https://www.youtube.com/watch?v=EYyOeZrMI9U
